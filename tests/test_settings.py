8647a79cffed94a14795751a8348bbaf6d2d3662
Arunderwood <arunderwood@users.noreply.github.com>
2017-03-22 23:13:50 -0500

    Pep8

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -11,12 +11,12 @@ test_files = os.path.join(dirname(os.path.realpath(__file__)), 'files')
 
 class TestSettings(TestCase):
 
-
     def test_load_settings(self):
-        settings_yaml = settings.load_settings(filename=os.path.join(test_files, 'test_settings1.yaml'))
+        settings_yaml = settings.load_settings(
+            filename=os.path.join(
+                test_files, 'test_settings1.yaml'))
         expected_result = {'Setting': {'File': 'Test'}}
         self.assertEquals(settings_yaml, expected_result)
-
 
     def test_save_settings(self):
         outfile = tempfile.mkstemp()[1]
