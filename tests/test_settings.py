3c915912453cf88f295fb65b6e50b018577f5502
Arunderwood <arunderwood@users.noreply.github.com>
2017-03-23 21:59:48 -0500

    Fix lint errors

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,23 +1,27 @@
+"""Test methods related to handling project settings."""
 from unittest import TestCase
+from os.path import dirname
 import os
 import tempfile
-from os.path import dirname
 from settings import settings
 
-tests_directory = dirname(os.path.realpath(__file__))
-test_files = os.path.join(dirname(os.path.realpath(__file__)), 'files')
+TEST_FILES = os.path.join(dirname(os.path.realpath(__file__)), 'files')
 
 
 class TestSettings(TestCase):
+    """Sets up tests"""
 
     def test_load_settings(self):
+        """Test load_settings which reads settings from a file."""
+
         settings_yaml = settings.load_settings(
             filename=os.path.join(
-                test_files, 'test_settings1.yaml'))
+                TEST_FILES, 'test_settings1.yaml'))
         expected_result = {'Setting': {'File': 'Test'}}
         self.assertEquals(settings_yaml, expected_result)
 
     def test_save_settings(self):
+        """Test save_settings which writes settings to a file."""
         outfile = tempfile.mkstemp()[1]
         expected = 'Setting:\n  File: Test\n'
         test_settings = {'Setting': {'File': 'Test'}}
@@ -25,7 +29,5 @@ class TestSettings(TestCase):
             settings.save_settings(test_settings, outfile)
             content = open(outfile).read()
         finally:
-            # NOTE: To retain the tempfile if the test fails, remove
-            # the try-finally clauses
             os.remove(outfile)
         self.assertEqual(content, expected)
